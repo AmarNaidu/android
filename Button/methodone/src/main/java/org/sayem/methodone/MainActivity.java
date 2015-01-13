23746d809ce5bcfd3f6fae047f1ba7dbca22e6ec
Syed Sayem <syed@sayem.org>
2015-01-12 21:33:58 -0500

    Implented OnClickListener Method One

---
+++ Button/methodone/src/main/java/org/sayem/methodone/MainActivity.java
@@ -0,0 +1,29 @@
+package org.sayem.methodone;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Log.d("Sayem", "Button was click");
+    }
+}
