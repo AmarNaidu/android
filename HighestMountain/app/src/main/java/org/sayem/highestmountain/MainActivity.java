901159e565e0848c90c39d1c42812bb7120e6039
Syed Sayem <syed@sayem.org>
2015-05-04 21:27:23 -0400

    Finish with HighestMountain app

--- HighestMountain/app/src/main/java/org/sayem/highestmountain/MainActivity.java
+++ HighestMountain/app/src/main/java/org/sayem/highestmountain/MainActivity.java
@@ -4,14 +4,38 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button showButton;
+    TextView showTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showButton = (Button) findViewById(R.id.buttonID);
+        showTextView = (TextView) findViewById(R.id.textViewID);
+
+
+        final String [] mountains = {"Everest", "Kangchenjunga", "Lhotse", "Makalu", "Dhaulagiri"};
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rand = new Random();
+                int r = rand.nextInt(mountains.length);
+                showTextView.setText(mountains[r]);
+            }
+        });
     }
 
 
