6f4d8526117d01b96239970aa7ce66f1c775cde7
Syed Sayem <syed@sayem.org>
2015-05-16 11:56:52 -0400

    Refactoring TempConverter app

--- TempConverter/app/src/main/java/org/sayem/tempconverter/MainActivity.java
+++ TempConverter/app/src/main/java/org/sayem/tempconverter/MainActivity.java
@@ -4,15 +4,48 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText tempEditText;
+    private Button celButton;
+    private Button fButton;
+    private TextView showTempTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempEditText = (EditText) findViewById(R.id.editText);
+        celButton = (Button) findViewById(R.id.cButton);
+        fButton = (Button) findViewById(R.id.fButton);
+        showTempTextView = (TextView) findViewById(R.id.titleView);
+
+        celButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+
+
+
 
 
     @Override
