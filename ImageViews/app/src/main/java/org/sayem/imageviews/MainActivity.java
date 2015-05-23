9d7773f660122a0a2cbb3c2ba366cdb3030403f9
Syed Sayem <syed@sayem.org>
2015-05-23 18:36:05 -0400

    Added images to ImageViewer app

--- ImageViews/app/src/main/java/org/sayem/imageviews/MainActivity.java
+++ ImageViews/app/src/main/java/org/sayem/imageviews/MainActivity.java
@@ -4,14 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ImageView appleImage;
+    private TextView showTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appleImage = (ImageView) findViewById(R.id.airdroid_image);
+        showTextView = (TextView) findViewById(R.id.image_name);
+
+        appleImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextView.setText("Airdroid is good");
+            }
+        });
     }
 
     @Override
