6cbaa6f0710f579c302b53474979c731b6cc72c6
Syed Sayem <syed@sayem.org>
2015-08-11 22:00:22 -0400

    Finish MyAppPortfolio app

--- MyAppPortfolio/app/src/main/java/org/sayem/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/org/sayem/myappportfolio/MainActivity.java
@@ -1,11 +1,14 @@
 package org.sayem.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +36,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void capstoneMyOwnApp(View view) {
+        Toast.makeText(getApplicationContext(),
+                "This button will launch my capstone app", Toast.LENGTH_LONG).show();
     }
 }
