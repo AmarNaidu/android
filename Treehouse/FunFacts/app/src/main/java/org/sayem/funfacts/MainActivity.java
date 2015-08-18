b8a53213152103876d00a3e5d3300843e5853477
Syed Sayem <syed@sayem.org>
2015-08-18 17:13:12 -0400

    Finish FunFacts app

--- Treehouse/FunFacts/app/src/main/java/org/sayem/funfacts/MainActivity.java
+++ Treehouse/FunFacts/app/src/main/java/org/sayem/funfacts/MainActivity.java
@@ -1,18 +1,12 @@
 package org.sayem.funfacts;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
