ab320b6204409dfedbe0294371a7554ce815b4ca
Syed Sayem <syed@sayem.org>
2015-08-27 11:28:03 -0400

    Parse integration

---
+++ Ribbit/app/src/main/java/org/sayem/ribbit/RibiitApplication.java
@@ -0,0 +1,23 @@
+package org.sayem.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by syed.sayem on 8/26/15.
+ */
+public class RibiitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "3xbB8PvEe07772aSsqrHZHslbYCfY2pFyldCmqrr", "zA2a7NvIRQd1cU6HjD3PwSpmXetSo3gYBo6odLH8");
+
+
+    }
+}
--- Ribbit/app/src/main/java/org/sayem/ribbit/SignUpActivity.java
+++ Ribbit/app/src/main/java/org/sayem/ribbit/SignUpActivity.java
@@ -1,16 +1,55 @@
 package org.sayem.ribbit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername = (EditText) findViewById(R.id.usernameField);
+        mPassword = (EditText) findViewById(R.id.passwordField);
+        mEmail = (EditText) findViewById(R.id.emailField);
+        mSignupButton = (Button) findViewById(R.id.signupButton);
+
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+                email = email.trim();
+
+                if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this)
+                            .setMessage(R.string.signup_error_message)
+                            .setTitle(R.string.signup_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+
+                }
+            }
+        });
+
     }
 
     @Override
