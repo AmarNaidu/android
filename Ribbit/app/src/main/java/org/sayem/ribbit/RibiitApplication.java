9757be7f34346df8452514815f35160b2dc98937
Syed Sayem <syed@sayem.org>
2015-09-04 13:07:42 -0400

    Update Ribiit App

--- Ribbit/app/src/main/java/org/sayem/ribbit/RibiitApplication.java
+++ Ribbit/app/src/main/java/org/sayem/ribbit/RibiitApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 /**
  * Created by syed.sayem on 8/26/15.
@@ -17,6 +18,7 @@ public class RibiitApplication extends Application {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "3xbB8PvEe07772aSsqrHZHslbYCfY2pFyldCmqrr", "zA2a7NvIRQd1cU6HjD3PwSpmXetSo3gYBo6odLH8");
+
 
 
     }
