e971e6c103c870fe28855109d486f102cf3b7f9c
Syed Sayem <syed@sayem.org>
2015-09-22 21:23:52 -0400

    Added Movies poster

--- code-examples/Seekbar/app/src/main/java/org/sayem/seekbar/MainActivity.java
+++ code-examples/Seekbar/app/src/main/java/org/sayem/seekbar/MainActivity.java
@@ -1,16 +1,42 @@
 package org.sayem.seekbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nicenessTextView;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nicenessTextView = (TextView) findViewById(R.id.nicenessTextView);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
