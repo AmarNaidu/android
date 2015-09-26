8a50bf961aa9abdd39249c1c85dcd1935d79ba94
Syed Sayem <syed@sayem.org>
2015-09-26 17:30:32 -0400

    Update PopularMovies app

--- PopularMovies/app/src/main/java/org/sayem/popularmovies/MainActivity.java
+++ PopularMovies/app/src/main/java/org/sayem/popularmovies/MainActivity.java
@@ -1,16 +1,28 @@
 package org.sayem.popularmovies;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGridView = (GridView) findViewById(R.id.moviesGridView);
     }
 
     @Override
@@ -33,5 +45,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class GridAdapter extends BaseAdapter{
+
+        ArrayList<Movies> mMovies;
+
+
+        public GridAdapter(Context context) {
+            mMovies = new ArrayList<>();
+            Resources resources = context.getResources();
+            String[] tempMovies = resources.getStringArray(R.array.movies);
+            int[] moviesImage = {
+                    R.drawable.americangangster,
+                    R.drawable.bladerunner,
+                    R.drawable.happygilmore,
+                    R.drawable.hugo,
+                    R.drawable.hulk,
+                    R.drawable.inception,
+                    R.drawable.pursuitofhappyness,
+                    R.drawable.reception,
+                    R.drawable.skyfall,
+                    R.drawable.threehundred,
+                    R.drawable.twilight,
+                    R.drawable.gamer};
+
+            for (int i = 0; i < moviesImage.length; i++){
+                Movies tempMovie = new Movies(moviesImage[i], tempMovies[i]);
+                mMovies.add(tempMovie);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
---
+++ PopularMovies/app/src/main/java/org/sayem/popularmovies/Movies.java
@@ -0,0 +1,15 @@
+package org.sayem.popularmovies;
+
+/**
+ * Created by syed.sayem on 9/26/15.
+ */
+public class Movies {
+
+    int imageId;
+    String movieName;
+
+    public Movies(int imageId, String movieName) {
+        this.imageId = imageId;
+        this.movieName = movieName;
+    }
+}
